26fae7a1f2706e1e02aaa490bb8b5203da08b02f
aashish0909 <illa.aashish999@gmail.com>
2021-09-07 16:08:39 +0530

    ADD - CORS

--- server.js
+++ server.js
@@ -2,6 +2,7 @@
 const express = require("express")
 const mongoose = require("mongoose")
 require("dotenv").config()
+const cors = require("cors")
 
 //Files
 const connectToMongo = require("./database")
@@ -9,6 +10,8 @@ const connectToMongo = require("./database")
 const app = express()
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
+app.use(cors())
+
 const port = process.env.PORT || 5000
 
 app.listen(port, () => {
