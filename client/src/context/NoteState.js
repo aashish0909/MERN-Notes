2ace87e9e9d59625e50d5e75923607312eba188f
aashish0909 <illa.aashish999@gmail.com>
2022-07-06 18:08:36 +0530

    minor changes

--- client/src/context/NoteState.js
+++ client/src/context/NoteState.js
@@ -1,80 +1,82 @@
-import NoteContext from "./NoteContext"
-import { useState } from "react"
-import axios from "axios"
+import NoteContext from "./NoteContext";
+import { useState } from "react";
+import axios from "axios";
 
 const NoteState = (props) => {
-	const host = ""
+  const host = "";
 
-	const notesInital = []
+  const notesInital = [];
 
-	const [notes, setNotes] = useState(notesInital)
+  const [notes, setNotes] = useState(notesInital);
 
-	const getNotes = async () => {
-		const json = await axios.get(`${host}/api/notes/fetchallnotes`, {
-			headers: {
-				"auth-token": localStorage.getItem("token"),
-			},
-		})
-		setNotes(Array.from(json.data))
-	}
+  const getNotes = async () => {
+    const json = await axios.get(`${host}/api/notes/fetchallnotes`, {
+      headers: {
+        "auth-token": localStorage.getItem("token"),
+      },
+    });
+    setNotes(Array.from(json.data));
+  };
 
-	const addNote = async (title, description, tag) => {
-		const response = await axios.post(
-			`${host}/api/notes/addnote`,
-			{
-				title: title,
-				description: description,
-				tag: tag,
-			},
-			{
-				headers: {
-					"auth-token": localStorage.getItem("token"),
-				},
-			}
-		)
+  const addNote = async (title, description, tag) => {
+    const response = await axios.post(
+      `${host}/api/notes/addnote`,
+      {
+        title: title,
+        description: description,
+        tag: tag,
+      },
+      {
+        headers: {
+          "auth-token": localStorage.getItem("token"),
+        },
+      }
+    );
 
-		setNotes([...notes, response.data])
-	}
+    setNotes([...notes, response.data]);
+  };
 
-	const deleteNote = async (id) => {
-		await axios.delete(`${host}/api/notes/deletenote/${id}`, {
-			headers: {
-				"auth-token": localStorage.getItem("token"),
-			},
-		})
-		const newNotes = notes.filter((note) => {
-			return note._id !== id
-		})
-		setNotes(newNotes)
-	}
+  const deleteNote = async (id) => {
+    await axios.delete(`${host}/api/notes/deletenote/${id}`, {
+      headers: {
+        "auth-token": localStorage.getItem("token"),
+      },
+    });
+    const newNotes = notes.filter((note) => {
+      return note._id !== id;
+    });
+    setNotes(newNotes);
+  };
 
-	const editNote = async (id, title, description, tag) => {
-		await axios.put(
-			`${host}/api/notes/updatenote/${id}`,
-			{ title, description, tag },
-			{
-				headers: {
-					"auth-token": localStorage.getItem("token"),
-				},
-			}
-		)
-		let newNotes = await JSON.parse(JSON.stringify(notes))
-		for (let i = 0; i < newNotes.length; i++) {
-			if (newNotes[i]._id === id) {
-				newNotes[i].title = title
-				newNotes[i].description = description
-				newNotes[i].tag = tag
-				break
-			}
-		}
-		setNotes(newNotes)
-	}
+  const editNote = async (id, title, description, tag) => {
+    await axios.put(
+      `${host}/api/notes/updatenote/${id}`,
+      { title, description, tag },
+      {
+        headers: {
+          "auth-token": localStorage.getItem("token"),
+        },
+      }
+    );
+    let newNotes = await JSON.parse(JSON.stringify(notes));
+    for (let i = 0; i < newNotes.length; i++) {
+      if (newNotes[i]._id === id) {
+        newNotes[i].title = title;
+        newNotes[i].description = description;
+        newNotes[i].tag = tag;
+        break;
+      }
+    }
+    setNotes(newNotes);
+  };
 
-	return (
-		<NoteContext.Provider value={{ notes, addNote, deleteNote, editNote, getNotes }}>
-			{props.children}
-		</NoteContext.Provider>
-	)
-}
+  return (
+    <NoteContext.Provider
+      value={{ notes, addNote, deleteNote, editNote, getNotes }}
+    >
+      {props.children}
+    </NoteContext.Provider>
+  );
+};
 
-export default NoteState
+export default NoteState;
