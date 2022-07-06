1e1c90453de808ffcf56ab2af7b999f3f6841b18
aashish0909 <illa.aashish999@gmail.com>
2022-07-06 17:42:59 +0530

    Fixed Heroku Errors

--- client/src/context/NoteState.js
+++ client/src/context/NoteState.js
@@ -3,7 +3,7 @@ import { useState } from "react"
 import axios from "axios"
 
 const NoteState = (props) => {
-	const host = "http://localhost:5000"
+	const host = ""
 
 	const notesInital = []
 
--- server.js
+++ server.js
@@ -1,34 +1,36 @@
 //Packages
-const express = require("express")
-const mongoose = require("mongoose")
-require("dotenv").config()
-const cors = require("cors")
-const path = require("path")
+const express = require("express");
+const mongoose = require("mongoose");
+require("dotenv").config();
+const cors = require("cors");
+const path = require("path");
 
 //Files
-const connectToMongo = require("./database")
+const connectToMongo = require("./database");
 
-const app = express()
-app.use(express.json())
-app.use(express.urlencoded({ extended: false }))
-app.use(cors())
+const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cors());
 
-const port = process.env.PORT || 5000
+const port = process.env.PORT || 5000;
 
 app.listen(port, () => {
-	console.log(`App listening at http://localhost:${port}`)
-})
+  console.log(`App listening at http://localhost:${port}`);
+});
 
-connectToMongo()
+connectToMongo();
 
 //Routes
-app.use("/api/auth", require("./routes/auth"))
-app.use("/api/notes/", require("./routes/notes"))
+app.use("/api/auth", require("./routes/auth"));
+app.use("/api/notes/", require("./routes/notes"));
 
-if (process.env.NODE_ENV === "production") {
-	console.log("!!!!YAY")
-	app.use(express.static(path.join(__dirname, "client", "build")))
-	app.get("*", (req, res) => {
-		res.sendFile(path.resolve(__dirname, "client", "build", "index.html"))
-	})
+if (
+  process.env.NODE_ENV === "production" ||
+  process.env.NODE_ENV === "staging"
+) {
+  app.use(express.static("client/build"));
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname + "/client/build/index.html"));
+  });
 }
